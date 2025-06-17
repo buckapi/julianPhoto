915c6b54426caa8480fc466a0f2711eda2118783
root <root@localhost.localdomain>
2025-06-17 11:59:59 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './components/about/about/about.component';
+import { ServiceComponent } from './components/service/service/service.component';
+import { GlobalService } from './services/global.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    AboutComponent,
+    ServiceComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'julianpwa';
+
+  constructor(public global: GlobalService) { }
 }
