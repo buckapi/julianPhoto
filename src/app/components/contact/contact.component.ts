ac33d17f6809a312ca21dd2670a401eaff260453
root <root@localhost.localdomain>
2025-06-28 15:23:18 -0500

    servicios y contacto

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,11 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  contact = {
+    name: '',
+    email: '',
+    comments: ''
+  };
 
+  sendToWhatsApp() {
+    const phone = '972526767187';
+    const message = `Hola, me contacto desde la web JulianPilcue.com, mi nombre es: ${this.contact.name}%0A, mi correo es ${this.contact.email}%0A y mi mensaje es: ${this.contact.comments}`;
+    const url = `https://wa.me/${phone}?text=${message}`;
+    window.open(url, '_blank');
+    this.resetForm()
+  }
+
+  resetForm() {
+    this.contact = {
+      name: '',
+      email: '',
+      comments: ''
+    };
+  }
 }
--- src/app/components/service/service/service.component.ts
+++ src/app/components/service/service/service.component.ts
@@ -1,22 +1,46 @@
 import { Component } from '@angular/core';
 import { GlobalService } from '../../../services/global.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-service',
   standalone: true,
-  imports: [CommonModule, ],
+  imports: [CommonModule, FormsModule ],
   templateUrl: './service.component.html',
   styleUrl: './service.component.css'
 })
 export class ServiceComponent {
 services: any[] = [];
+item: any = {};
+searchTerm: string = '';
 
+get filteredServices() {
+  if (!this.searchTerm) return this.services;
+  return this.services.filter(service =>
+    service.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+  );
+}
 constructor(public global: GlobalService) {}
 
-ngOnInit(): void {
-  this.global.services$.subscribe((services : any[]) => {
-    this.services = services;
-  });
-}
+  ngOnInit(): void {
+    this.global.services$.subscribe((services : any[]) => {
+      this.services = services;
+    });
+  }
+
+  sendToWhatsApp(item: any) {
+    const phone = '972526767187';
+    const message = `Hola, estoy interesad@ en el servicio: ${item.name},%0APrecio: ${item.price}`;
+    const url = `https://wa.me/${phone}?text=${message}`;
+    window.open(url, '_blank');
+    this.resetForm()
+  }
+
+  resetForm() {
+    this.item = {
+      name: '',
+      price: ''
+    };
+  }
 }
