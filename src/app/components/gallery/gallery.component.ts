1125606342235e5973dca6b7cea07cea4f904e5f
root <root@localhost.localdomain>
2025-06-28 15:37:16 -0500

    imagenes

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,23 +1,41 @@
 import { Component } from '@angular/core';
 import { GlobalService } from '../../services/global.service';
 import { CommonModule } from '@angular/common';
+import { LightboxModule, Lightbox } from 'ngx-lightbox';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LightboxModule],
+  providers: [Lightbox],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
 export class GalleryComponent {
 
   gallery: any[] = [];
-  
-  constructor(public global: GlobalService) { }
+  lightboxImages: any[] = [];
+
+  constructor(public global: GlobalService,
+     private lightbox: Lightbox) { }
 
   ngOnInit(): void {
     this.global.gallery$.subscribe((gallery : any[]) => {
       this.gallery = gallery;
+      this.lightboxImages = gallery.map(gal => ({
+        src: gal.files[0],
+        caption: gal.name,
+        thumb: gal.files[0]
+      }));
     });
   }
+
+  open(index: number): void {
+    this.lightbox.open(this.lightboxImages, index);
+  }
+
+  close(): void {
+    this.lightbox.close();
+  }
 }
+
